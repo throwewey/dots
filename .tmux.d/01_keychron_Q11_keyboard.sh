e0e843aaa037eb66e426f3284bde7ab7613d7d4d
throwewey <throwewey@environment>
2026-03-10 15:00:09 -0600

    added docking station keyboard and monitor

---
+++ .tmux.d/01_keychron_Q11_keyboard.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [[ -e "/usr/bin/kmonad" && -e "dev/input/by-id/usb-Keychron_Keychron_Q11_59006800075041525233322000000000-if01-event-kbd" ]]; then
+	kmonad ~/.config/kmonad/keychron_Q11.kbd >/dev/null 2>&1 &
+	echo "keychron Q11 found, starting kmonad for keychron Q11 keyboard..."
+fi
+
+
